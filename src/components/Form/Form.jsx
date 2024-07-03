5c5d2e7456167e879197768f854c95fc89e3d324
VadimDubovik <dubovik_03@inbox.ru>
2024-07-03 23:39:10 +0700

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,9 +18,9 @@ const Form = () => {
     },[])
 
     useEffect(() =>{
-        tg.onEvent('mainButtonClick',onSendData)
+        tg.onEvent('mainButtonClicked',onSendData)
         return ()=>{
-            tg.offEvent('mainButtonClick',onSendData)
+            tg.offEvent('mainButtonClicked',onSendData)
         }
     },[])
 
